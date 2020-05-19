4b927f001f86da7358bac6d0bde6f91bbc205042
coderase <maxime.porpiglia@gmail.com>
2020-05-19 08:27:41 +0200

    Premier commit

---
+++ MAD.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: MAD
+Description: V2 MAD
+Author: Maxime PORPIGLIA
+Version: 1
+*/
+
+if(!defined('ABSPATH')):
+  die;
+endif;
+
+define('PLUGIN_MAD_DIRECTORY', plugin_dir_path(__FILE__));
+define('PLUGIN_MAD_URL', plugins_url());
+
+if(!class_exists('MAD')):
+  class MAD{
+    function __construct(){
+      require(PLUGIN_MAD_DIRECTORY.'vendor/autoload.php');
+
+      add_action('init', array($this, 'register_custom_post_type'));
+      register_activation_hook( __FILE__ , array('MAD', 'install'));
+      register_uninstall_hook( __FILE__ , array('MAD', 'uninstall'));
+    }
+
+    public function register_custom_post_type(){
+
+    }
+
+    public static function install(){
+      global $wpdb;
+    }
+
+    public static function uninstall(){
+      global $wpdb;
+    }
+  }
+endif;
+
+new MAD();
