548ca4cbbb315ba519f2f8890480cafb0c72457d
coderase <maxime.porpiglia@gmail.com>
2020-06-05 09:42:46 +0200

    shortcode header v3

--- inc/shortcodes/header/header.php
+++ inc/shortcodes/header/header.php
@@ -1,23 +1,23 @@
 <div id="shortcode_header">
-  <div class="col">
+  <div class="col1">
     <a><img src="<?php echo PLUGIN_MAD_URL.'/MAD/img/Zoom.png'; ?>"/></a>
   </div>
 
-  <div class="col">
+  <div class="col2">
     <a>i'm a mad artist</a>
   </div>
 
-  <div class="col">
+  <div class="col1">
     <a>sign in</a>
   </div>
 
-  <div class="col">
+  <div class="col1">
     <img src="<?php echo PLUGIN_MAD_URL.'/MAD/img/Shop.png'; ?>"/>
   </div>
 
   <div class="separator"></div>
 
-  <div class="col">
+  <div class="col1">
     <a>EN</a> / <a>FR</a>
   </div>
 </div>
